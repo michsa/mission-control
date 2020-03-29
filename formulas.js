07605593e21bf0c31d4fc742d1590d3aaf5a9619
Michelle Saad <git@michsa.me>
2020-03-29 14:24:42 -0400

    add notes on payload, impulse/thrust/burn calculations

--- formulas.js
+++ formulas.js
@@ -1,9 +1,7 @@
 const Qty = require('js-quantities')
 
-/* Physics formulas and constants to make rocket go. This file should contain only pure functions. */
-
-// https://en.wikipedia.org/wiki/RP-1
-const fuelDensity = new Qty(0.9, 'g/ml')
+// Physics formulas and constants to make rocket go.
+// This file should contain only pure functions.
 
 // https://en.wikipedia.org/wiki/Earth_radius
 const radiusOfEarth = new Qty(6370, 'km')
@@ -17,10 +15,17 @@ const calcGravitationalAcceleration = height => {
 const calcWeightAtAltitude = (mass, height) =>
   mass.mul(calcGravitationalAcceleration(height))
 
+const calcThrust = (impulse, massFlowRate) =>
+  impulse.mul(massFlowRate).mul('1 gee')
+
 // we need a rough approximate for the rocket's thrust - the real formulas are
 // too complex since they depend on all sorts of propertes of the fuel & rocket
 const calcForce = (thrust, weight) => thrust.sub(weight)
 
 const calcAcceleration = (force, mass) => force.div(mass)
 
-module.exports = { fuelDensity }
+module.exports = { calcForce, calcAcceleration }
+
+// impulse (m/s) = thrust (N or kgm/s^2) / mass flow rate (kg/s)
+// specific impulse (s) = thrust (N or kgm/s^2) / weight flow rate (N/s or kgm/s^3)
+// thrust (N or kgm/s^2) = impulse (s) * mass flow rate (kg/s) * gravitation acceleration (m/s^2)
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Qty = require('js-quantities')
-const { fuelDensity } = require('./formulas')
+const { calcForce, calcAcceleration } = require('./formulas')
 
 let seed = 1
 let random = () => {
@@ -16,17 +16,27 @@ const updateInterval = 100
 // --- parameters ---
 
 let distance = new Qty(160, 'km')
+// NOTE: The readme says this 50,000 kg payload includes the fuel, but that's
+// likely an error -- one liter of rocket fuel (or water) weighs roughly 1 kg,
+// so our total payload including fuel would exceed 1 million kilograms!
+// That actually sounds more realistic for a rocket, so I'm going to assume
+// that the fuel is *NOT* included and that this value is the actual payload
+// (which will make calculations easier anyway).
 let payload = new Qty(50000, 'kg')
 let fuel = new Qty(1514100, 'l')
-let targetBurnRate = new Qty(168240, 'l/m')
+let fuelDensity = new Qty(0.9, 'g/ml') // https://en.wikipedia.org/wiki/RP-1
+let burnRate = new Qty(168240, 'l/s')
 let targetSpeed = new Qty(1500, 'km/h')
+// Specific impulse is a measure of the efficiency of a rocket and allows us to
+// calculate thrust from the burn rate (or "mass flow rate") of our fuel.
+// https://en.wikipedia.org/wiki/Specific_impulse#Specific_impulse_in_seconds
+let impulse = new Qty(500, 's')
 
 // --- mission state ---
 
 let currentFuel = new Qty(fuel)
 let currentSpeed = new Qty(0, 'km/h')
 let averageSpeed = new Qty(0, 'km/h')
-let currentBurnRate = new Qty(0, 'l/m')
 let distanceTraveled = new Qty(0, 'km')
 let timeElapsed = new Qty(0, 's')
 
@@ -47,7 +57,9 @@ const calcAverageSpeed = () => {
     .div(samples.add('1'))
 }
 
-const calcMass = state => payload.sub(fuel.sub(currentFuel).div(fuelDensity))
+const calcMass = () => payload.add(currentFuel.mul(fuelDensity))
+
+const calcThrust = () => impulse.mul(burnRate.mul(fuelDensity).mul('1 gee'))
 
 // --- output formatting ---
 
@@ -58,7 +70,7 @@ Mission plan:
   Travel distance:  ${distance}
   Payload capacity: ${payload}
   Fuel capacity:    ${fuel}
-  Target burn rate: ${targetBurnRate}
+  Target burn rate: ${burnRate}
   Target speed:     ${targetSpeed}
   Random seed:      12
 `)
@@ -68,13 +80,16 @@ const missionStatus = () => {
   console.log(`
 Mission status:
   Elapsed time:        ${timeElapsed.toPrec(1)}
-  Fuel burn rate:      ${currentBurnRate}
+  Fuel burn rate:      ${burnRate}
   Current speed:       ${currentSpeed.toPrec(0.1)}
   Average speed:       ${averageSpeed.toPrec(0.1)}
   Distance traveled:   ${distanceTraveled.toPrec(0.1)}
   Time to destination: ${calcTimeToDestination()
     .to('s')
     .toPrec(0.01)}
+  ---
+  Mass: ${calcMass()}
+  Thrust: ${calcThrust()}
 `)
 }
 
@@ -84,16 +99,18 @@ const runMission = () => {
   startMission()
 
   timer = setInterval(() => {
+    let interval = new Qty(updateInterval, 'ms')
+
     currentSpeed = currentSpeed.add(new Qty(10, 'm/s'))
     averageSpeed = calcAverageSpeed()
-    distanceTraveled = distanceTraveled.add(
-      currentSpeed.mul(new Qty(updateInterval, 'ms'))
-    )
-    timeElapsed = timeElapsed.add(new Qty(updateInterval, 'ms'))
+    distanceTraveled = distanceTraveled.add(currentSpeed.mul(interval))
+
+    timeElapsed = timeElapsed.add(interval)
+    currentFuel = currentFuel.sub(burnRate.mul(interval))
 
     missionStatus()
 
-    if (distanceTraveled.gte(distance)) clearInterval(timer)
+    if (distanceTraveled.gte(distance) || currentFuel.lte('0 l')) clearInterval(timer)
   }, updateInterval)
 }
 
